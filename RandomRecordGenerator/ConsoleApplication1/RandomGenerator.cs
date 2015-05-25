c67330bccd0791d55db425882d6c2faae9fe3138
jnelso117 <jcn1990s@gmail.com>
2015-05-25 14:59:22 -0500

    First Commit.
    What needs to be done:
    - Overall, more documentation.
    - Currently prepping a Python script that accesses a name generator
    site, generates n amount of names, and saves them to each respected
    name.txt file.
    - Without the Python script or someone adding more names to the
    names.txt files, the program will crash after 30 records are created.
    -More method overloading in RandomGenerator.cs
    
    PURPOSE: Entire purpose is to make a project that allows students to
    enter some realistic dummy tuples into a database for educational
    purposes.  I built this for my DBMS class, and we used an Access DB for
    our data, unfortunately.  This can be changed by the user easily, as
    Visual Studio can connect to SQL databases rather well.  If anyone uses
    this in a real DB, please ensure that you encrypt your DB properly

---
+++ RandomRecordGenerator/ConsoleApplication1/RandomGenerator.cs
@@ -0,0 +1,181 @@
+﻿/// <Summary>
+///  @author Jeremy Nelson @ The University of Alabama at Birmingham.
+///  This is a Random Record Generator API to create Dummy test tuples into a database.  I wrote this during my DBMS class.
+///  Professor: Dr. Samuel Thompson
+///  My Project: Infant Mortality
+///  Team Members: Neeti Thakral, Michael McGrath, and Rolando Davis.
+///  Upon completing my sample Database for class, I decided to clean it up, extend it a litle bit more, and publish for other students to use
+/// </Summary  
+ 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class RandomGenerator
+    {
+        //Use this static random object for all the methods
+        static Random _random = new Random();
+
+        /// <randomSKU>
+        /// No Parameters
+        /// Returns a random String of numbers and alphanumerical values.
+        /// </randomSKU>
+        /// <returns>
+        /// Returns a String value and a length of 10 characters
+        /// </returns>
+        public static String randomSKU()
+        {
+            //Randomizes to get the SKU
+            Random random = new Random();
+            Double number = random.Next(0, 10);
+            Double stringLength = 10;
+            String allowedChars = "";
+            allowedChars = "1,2,3,4,5,6,7,8,9,0";
+            allowedChars += "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,";
+            char[] token = { ',' };
+            String[] characters = allowedChars.Split(token);
+            String temp = "";
+            String stringID = "";
+            String sku = "";
+            for (int i = 0; i < stringLength; i++)
+            {
+                temp = characters[(random.Next(0, characters.Length))];
+                stringID += temp;
+                sku = stringID;
+            }
+            return sku;
+        }
+        /// <randomSKU(int N)>
+        /// Generates a random String of numbers and alphanumerical values.
+        /// </randomSKU(int N>
+        /// <param name="N">
+        /// Length of the SKU
+        /// </param>
+        /// <returns>Returns a String value of N length</returns>
+        public static String randomSKU(int N)
+        {
+            //Randomizes to get the SKU
+            Random random = new Random();
+            Double number = random.Next(0, 10);
+            Double stringLength = N;
+            String allowedChars = "";
+            allowedChars = "1,2,3,4,5,6,7,8,9,0";
+            allowedChars += "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,";
+            char[] token = { ',' };
+            String[] characters = allowedChars.Split(token);
+            String temp = "";
+            String stringID = "";
+            String sku = "";
+            for (int i = 0; i < stringLength; i++)
+            {
+                temp = characters[(random.Next(0, characters.Length))];
+                stringID += temp;
+                sku = stringID;
+            }
+            return sku;
+        }
+        /// <randomEmail>
+        /// Generates a random E-mail with a maximum length of 10 characters before the host value.
+        /// Characters used in the generation are are 123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        /// Possible host values: @gmail.com, @hotmail.com, @yahoo.com
+        /// </summary>
+        /// <returns> Returns a String with random 10 random characters + hostname</returns>
+        public static String randomEmail()
+        {
+            //Generates Random E-mail with the character limit being 10 before the host is added to the end of it.  
+            Random random = new Random();
+            Double number = random.Next(0, 10);
+            String[] hosts = new String[] { "@gmail.com", "@hotmail.com", "@yahoo.com" };
+            Double stringLength = 10;
+            String allowedChars = "";
+            allowedChars = "1,2,3,4,5,6,7,8,9,0";
+            allowedChars += "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,";
+            char[] token = { ',' };
+            String[] characters = allowedChars.Split(token);
+            String temp = "";
+            String stringID = "";
+            String email = "";
+            for (int i = 0; i < stringLength; i++)
+            {
+                temp = characters[(random.Next(0, characters.Length))];
+                stringID += temp;
+                email = stringID;
+            }
+
+            return email + shuffleReturn(hosts);
+
+        }
+        public static String randomPhone()
+        {
+            //Random phone number.  Phone number has a prefix, suffix, and an extension (typically).  
+            Random r = new Random();
+            int prefix = r.Next(100, 999);
+            int suffix = r.Next(100, 999);
+            int extension = r.Next(1000, 9999);
+            return prefix.ToString() + "-" + suffix.ToString() + "-" + extension.ToString();
+        }
+
+        public static String randomPassword()
+        {
+            Double passwordLength = 12;
+            String allowedChars = "";
+            String password = "";
+            allowedChars = "1,2,3,4,5,6,7,8,9,0";
+            allowedChars += "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            allowedChars += "!,@,#,$,%,^,*,(,),_,`,~,?";
+            char[] token = { ',' };
+            //Using the , delimiter, allowedChars is broken up into an array of all the characters listed above
+            String[] characters = allowedChars.Split(token);
+
+            String temp = "";
+            String stringID = "";
+            Random r = new Random();
+            for (int i = 0; i < passwordLength; i++)
+            {
+                temp = characters[r.Next(0, characters.Length)];
+                stringID += temp;
+                password = stringID;
+            }
+            return password;
+        }
+        public static void Shuffle<T>(T[] array)
+        {
+            //Fisher-Yates Shuffle Algorithm to Shuffle an Array.
+            //Shuffles contents in an array.  
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+
+                int r = i + (int)(_random.NextDouble() * (n - i));
+                T t = array[r];
+                array[r] = array[i];
+                array[i] = t;
+            }
+        }
+        public static String shuffleReturn<T>(T[] array)
+        {
+            //Shuffles an array and returns the first element in the array as a String.  
+            Shuffle(array);
+            try
+            {
+                return array[0].ToString();
+            }
+            catch (Exception e)
+            {
+                throw new System.ArgumentException("Please ensure that the array length is greater than 0");
+            }
+        }
+        public static int randomNumber(int min, int max)
+        {
+            Random r = new Random();
+            int number = r.Next(min, max);
+            return number;
+        }
+    }
+}
---
+++ RandomRecordGenerator/ConsoleApplication1/RandomRecordGenerator.cs
@@ -0,0 +1,184 @@
+﻿/*******************************
+ * Program: RandomRecordGenerator
+ * Author: Jeremy Nelson, UAB
+ * E-mail: jcn1990s@gmail.com
+ * 
+ * Info: This program was made for the sole purpose of writing random "dummy" data into a database.  Our limit was 30 records per Table for our Sponsor.
+ * I decided to publish this code because it could come in handy for the next batch of students taking IS301 or wanting to dabble with using OleDBConnection to
+ * write inside a database.  
+ * This project consists of .txt files with 30 records each:
+ * - Text file consisting of 30 Female names.
+ * - Text file consisting of 30 Male names.
+ * - Text file consisting of 30 Last names.
+ * - Text File consisting of 30 names of Popular Operating Systems excluding Windows Phone past 8.1
+ * - Text file consisting of popular Vendor names, with many repeating names.
+ * 
+ * If you would like to add more names, feel free to do so.  If you use my code, please give me shoutout of some sort. If you see anything that I could fix, please
+ * let me know.  
+ * 
+ * Hopefully, this can get some beginning programmers a base to start with in for learning how to use Connection Strings and get the imagination juices flowing.
+ * 
+ * Ideas moving forward: Making a UI for the whole thing or using Command Line arguments for input. Finding a way to randomize the data in a way such that you won't
+ * receive an Exception for going out of bounds of your Input. TL;DR, write a way to randomize the names that it guarantees no duplicates and no OutOfBounds Exceptions.
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.OleDb;
+namespace ConsoleApplication1
+{
+   public class RandomRecordGenerator
+    {
+        static void Main(string[] args)
+        {
+            //Initialize some arrays that will be used
+            String[] wFirstNames = System.IO.File.ReadAllLines("WFirstNames.txt");
+            String[] mFirstNames = System.IO.File.ReadAllLines("MFirstNames.txt");
+            String[] LastNames = System.IO.File.ReadAllLines("LastNames.txt");
+            String[] vendors = System.IO.File.ReadAllLines("Vendors.txt");
+            String[] compatibility = System.IO.File.ReadAllLines("Compatibility.txt");
+            String[] status = { "See Doctor Immediately", "Normal", "Schedule Appointment" };
+            String[] popularOS = new String[]{"Android", "Apple iOS", "Microsoft"};
+            String[] specialties = { "Surgeon", "Physician", "Gynecologist" };
+
+
+           /* using (OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Nagato\Documents\InfantMortality.accdb;
+Persist Security Info=False")) {
+                //You need to change the OleDbConnection String to fit your File Source for your database.  This also refers to a Database not being password protected.
+                //You may add your Connection String via Project > Add New Data Source and follow the instructions.
+            * Next, open the connection
+            connection.Open();   
+            //My Assignment was to generate 30 random records into our Tables. 
+            * Consider making this number take whatever param you need via Console.ReadLine
+            /* WARNINGS BEFORE DOING YOUR QUERIES:
+             * IF YOUR QUERY(S) IS/ARE WRITING PK'S, AND YOU RUN THE QUERY AGAIN AFTER THE PK VALUES ARE GENERATED, YOU WILL GET A RUNTIME ERROR FOR DUPLICATES IN THE COLUMN.
+             * JUST BE CAREFUL WHEN TESTING WITH PK'S.
+             */
+            for(int i = 0; i < 30; i++)
+            {
+                
+                
+                //Shuffle the information.
+                RandomGenerator.Shuffle(wFirstNames);
+                RandomGenerator.Shuffle(mFirstNames);
+                RandomGenerator.Shuffle(LastNames);
+                RandomGenerator.Shuffle(vendors);
+                RandomGenerator.Shuffle(compatibility);
+
+                //Query to PATIENT table
+                OleDbCommand patientQueryCommand = new OleDbCommand();
+                patientQueryCommand.Connection = connection;
+                patientQueryCommand.CommandText = "INSERT INTO PATIENT (PATIENT_EMAIL, PATIENT_FNAME, PATIENT_LNAME, PATIENT_WEIGHT, PATIENT_HEIGHT, PATIENT_TRIMESTER, PATIENT_PASSWRD, CONDITION_ID) VALUES (?,?,?,?,?,?,?,?)";
+                patientQueryCommand.Parameters.Add("PATIENT_EMAIL", OleDbType.LongVarChar);
+                patientQueryCommand.Parameters.Add("PATIENT_FNAME", OleDbType.LongVarChar);
+                patientQueryCommand.Parameters.Add("PATIENT_LNAME", OleDbType.LongVarChar);
+                patientQueryCommand.Parameters.Add("PATIENT_WEIGHT", OleDbType.Numeric);
+                patientQueryCommand.Parameters.Add("PATIENT_HEIGHT", OleDbType.Numeric);
+                patientQueryCommand.Parameters.Add("PATIENT_TRIMESTER", OleDbType.Numeric);
+                patientQueryCommand.Parameters.Add("PATIENT_PASSWRD", OleDbType.LongVarChar);
+                patientQueryCommand.Parameters.Add("CONDITION_ID", OleDbType.LongVarChar);
+                patientQueryCommand.Parameters[0].Value = RandomGenerator.randomEmail();
+                patientQueryCommand.Parameters[1].Value = wFirstNames[i];
+                patientQueryCommand.Parameters[2].Value = LastNames[i];
+                patientQueryCommand.Parameters[3].Value = RandomGenerator.randomNumber(130, 140);
+                patientQueryCommand.Parameters[4].Value = RandomGenerator.randomNumber(66, 70);
+                patientQueryCommand.Parameters[5].Value = RandomGenerator.randomNumber(1, 3);
+                patientQueryCommand.Parameters[6].Value = RandomGenerator.randomPassword();
+                patientQueryCommand.Parameters[7].Value = RandomGenerator.shuffleReturn(status);
+
+                //Query to PERIPHERAL Table
+                OleDbCommand peripheralQueryCommand = new OleDbCommand();
+                peripheralQueryCommand.Connection = connection;
+                peripheralQueryCommand.CommandText = "INSERT INTO PERIPHERAL (P_FDA_NUM, P_SYS_COMPAT) VALUES (?,?)";
+                peripheralQueryCommand.Parameters.Add("P_FDA_NUM", OleDbType.Numeric);
+                peripheralQueryCommand.Parameters.Add("P_SYS_COMPAT", OleDbType.LongVarChar);
+                peripheralQueryCommand.Parameters[0].Value = RandomGenerator.randomNumber(1000000, 1500000);
+                peripheralQueryCommand.Parameters[1].Value = RandomGenerator.shuffleReturn(compatibility);
+               
+                //Query to VENDOR Table
+                OleDbCommand venderQueryCommand = new OleDbCommand();
+                venderQueryCommand.Connection = connection;
+                venderQueryCommand.CommandText = "INSERT INTO VENDOR (P_SYS_COMPAT, P_FDA_NUM) VALUES (?,?)";
+                venderQueryCommand.Parameters.Add("P_SYS_COMPAT", OleDbType.LongVarChar);
+                venderQueryCommand.Parameters.Add("P_FDA_NUM", OleDbType.Numeric);
+                venderQueryCommand.Parameters[0].Value = RandomGenerator.shuffleReturn(compatibility);
+                venderQueryCommand.Parameters[1].Value = RandomGenerator.randomNumber(1000000, 1500000);
+
+                //Query to COMPATIBILITY Table
+                OleDbCommand compatibilityQueryCommand = new OleDbCommand();
+                compatibilityQueryCommand.Connection = connection;
+                compatibilityQueryCommand.CommandText = "INSERT INTO COMPATIBILITY (DEVICE_OS, DEVICE_COMPATIBILITY) VALUES (?,?)";
+                compatibilityQueryCommand.Parameters.Add("DEVICE_OS", OleDbType.LongVarChar);
+                compatibilityQueryCommand.Parameters.Add("DEVICE_COMPATIBILITY", OleDbType.LongVarChar);
+                compatibilityQueryCommand.Parameters[0].Value = compatibility[i];
+                if (compatibility[i].Contains("Android"))
+                {
+                    compatibilityQueryCommand.Parameters[1].Value = "Android";
+                }
+                else if (compatibility[i].Contains("Apple"))
+                {
+                    compatibilityQueryCommand.Parameters[1].Value = "Apple";
+                }
+                else if (compatibility[i].Contains("Windows"))
+                {
+                    compatibilityQueryCommand.Parameters[1].Value = "Microsoft";
+                }
+                else compatibilityQueryCommand.Parameters[1].Value = "Unknown";
+
+                // Query for DEVICE Table
+
+                OleDbCommand deviceQueryCommand = new OleDbCommand();
+                deviceQueryCommand.Connection = connection;
+                deviceQueryCommand.CommandText = "INSERT INTO DEVICE (DEVICE_SKU, DEVICE_VENDOR) VALUES (?,?)";
+                deviceQueryCommand.Parameters.Add("DEVICE_SKU", OleDbType.LongVarChar);
+                deviceQueryCommand.Parameters.Add("Device_Vendor", OleDbType.LongVarChar);
+                deviceQueryCommand.Parameters[0].Value = RandomGenerator.randomSKU();
+                deviceQueryCommand.Parameters[1].Value = vendors[i];
+
+                //Query for DOCTOR Table
+                OleDbCommand doctorQueryCommand = new OleDbCommand();
+                doctorQueryCommand.Connection = connection;
+                doctorQueryCommand.CommandText = "INSERT INTO DOCTOR (SPECIALTY_1, SPECIALTY_2, SPECIALTY_3, DOC_CONTACT) VALUES (?,?,?,?)";
+                doctorQueryCommand.Parameters.Add("SPECIALTY_1", OleDbType.LongVarChar);
+                doctorQueryCommand.Parameters.Add("SPECIALTY_2", OleDbType.LongVarChar);
+                doctorQueryCommand.Parameters.Add("SPECIALTY_3", OleDbType.LongVarChar);
+                doctorQueryCommand.Parameters.Add("DOC_CONTACT", OleDbType.LongVarChar);
+                doctorQueryCommand.Parameters[0].Value = RandomGenerator.shuffleReturn(specialties);
+                doctorQueryCommand.Parameters[1].Value = RandomGenerator.shuffleReturn(specialties);
+                doctorQueryCommand.Parameters[2].Value = RandomGenerator.shuffleReturn(specialties);
+                doctorQueryCommand.Parameters[3].Value = RandomGenerator.randomPhone();
+
+                //Queries are not pushed until the ExecuteNonQuery method is provoked.
+                 //* I recommend writing try-catch statements here, but I didn't do it because I didn't know what error I should catch.
+                patientQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Patient records generated.");
+                peripheralQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Peripheral records generated");
+                venderQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Vendor records generated");
+                //May get an error if it tries to commit the same values for PK.
+                compatibilityQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Compatibility records generated");
+                deviceQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Device records generated");
+                doctorQueryCommand.ExecuteNonQuery();
+                Console.WriteLine("Doctor records generated");
+
+                
+
+
+
+
+            }
+            //Always close string connection
+            //connection.Close();
+        }
+            //Console.WriteLine("Contents pushed successfully");
+            //Console.ReadLine();
+            
+        }
+    }
+//}
